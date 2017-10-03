ca602bffee88b30edb2d6cffbaa8279baaa555cd
Edel John Ingal <ejingal@gmail.com>
2017-10-03 01:03:07 +0000

    DRY loopylighthouse

--- loopyLighthouse.js
+++ loopyLighthouse.js
@@ -1,4 +1,4 @@
-for (var i = 100; i <= 200; i += 1)
+/*for (var i = 100; i <= 200; i += 1)
   if (i % 12 === 0)
     console.log("LoopyLighthouse");
   else if (i % 3 === 0)
@@ -7,3 +7,12 @@ for (var i = 100; i <= 200; i += 1)
     console.log("Lighthouse");
   else
     console.log(i);
+  */
+
+
+// DRY version
+
+for (var i = 100; i <= 200; i += 1){
+  var lo = i % 3 == 0, li = i % 5 == 0;
+  console.log(lo ? li ? "LoopyLighthouse" : "Loopy" : li ? "Lighthouse" : i);
+}
